499b212f005de464c6f1998fddfefc22a95aae37
bokelai1989 <38841207+bokelai1989@users.noreply.github.com>
2021-02-23 09:32:45 -0800

    Update A wolf in sheeps clothing.py

--- A wolf in sheeps clothing.py
+++ A wolf in sheeps clothing.py
@@ -1,6 +1,17 @@
+# original order 
 def warn_the_sheep(queue):
     if queue[-1] == "wolf":
         return 'Pls go away and stop eating my sheep'
     else: 
         
         return 'Oi! Sheep number {}! You are about to be eaten by a wolf!'.format(len(queue) - queue.index("wolf")-1 )
+
+    
+# reversed order     
+def warn_the_sheep(queue):    
+    queue.reverse()
+    wolfnum = queue.index("wolf")
+    if wolfnum == 0:
+        return "Pls go away and stop eating my sheep"
+    else:
+        return "Oi! Sheep number {0}! You are about to be eaten by a wolf!".format(wolfnum)
