4cf0b830bf9fbdde0da37e8c8a4fb317a4e835d4
bokelai1989 <38841207+bokelai1989@users.noreply.github.com>
2021-02-24 09:25:25 -0800

    Update Find the stray number.py

--- Find the stray number.py
+++ Find the stray number.py
@@ -7,3 +7,13 @@ def stray(arr):
         return list(arr_set)[1]
     else:
         return list(arr_set)[0]
+
+    
+    
+# use count function 
+def stray(arr): 
+    
+    for x in set(arr):  # using set here is more efficient 
+        if arr.count(x) == 1:
+            return x 
+        
